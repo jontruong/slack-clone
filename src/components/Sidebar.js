cf2896be65a348be537748cbcdc4a18d4cc6e971
Jonathan Truong <jjtruong90@gmail.com>
2021-04-13 12:24:10 -0700

    Adds Css to header

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div style= {{ background: "green"}}>
+            sidebar
+        </div>
+    )
+}
+
+export default Sidebar
